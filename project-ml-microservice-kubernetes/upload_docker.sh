65b7a608354d6af8d72487764dd1e9381a80b661
dpeasant <dancingpeasant@gmail.com>
2022-07-01 15:27:05 -0700

    Update upload_docker.sh

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=dpeasant/pro4
 
 # Step 2:  
 # Authenticate & tag
+docker login --username dpeasant
+docker tag pro4 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
