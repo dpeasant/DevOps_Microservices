cd5b6ec39a3f49aeac9c358506c43b961a9c05ef
dpeasant <dancingpeasant@gmail.com>
2022-07-01 15:48:54 -0700

    Update run_docker.sh

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build . --tag pro4
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker container run -p 8000:80 pro4
